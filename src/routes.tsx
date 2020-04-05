f80e639611333cfb4d57aeb0bd5d09607738c2f8
Italo <italo_bruno91@hotmail.com>
2020-04-05 18:46:42 -0300

    :sparkles: feat: add stack header

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
 
 import BillScreen from "./screens/BillScreen";
 import CrustScreen from "./screens/CrustScreen";
@@ -19,7 +20,27 @@ const Stack = createStackNavigator<StackParamList>();
 const Routes = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#ffc100",
+            shadowColor: "transparent",
+            elevation: 0,
+          },
+          headerTitle: "",
+          headerBackImage: () => (
+            <Ionicons
+              name="ios-arrow-back"
+              size={35}
+              style={{
+                color: "#fff",
+                paddingLeft: 20,
+              }}
+            />
+          ),
+          headerBackTitle: " ",
+        }}
+      >
         <Stack.Screen name="Size" component={SizeScreen} />
         <Stack.Screen name="Crust" component={CrustScreen} />
         <Stack.Screen name="Toppings" component={ToppingsScreen} />

5b1517bf85b7acd394ef8d12d11bf8d6ae7ecb8d
Italo <italo_bruno91@hotmail.com>
2020-04-05 18:50:09 -0300

    :lipstick: style: change statusbar color

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { StatusBar } from "react-native";
 
 import Routes from "./src/routes";
 import { Provider } from "./src/context/PizzaContext";
 
-export default function App() {
+const App = () => {
   return (
     <Provider>
+      <StatusBar barStyle="light-content" backgroundColor="#ffc100" />
       <Routes />
     </Provider>
   );
-}
+};
+
+export default App;

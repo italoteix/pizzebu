5296454549dd3d0c244cd4ed9563ae85eb3b9c5c
Italo <italo_bruno91@hotmail.com>
2020-04-05 13:10:50 -0300

    :sparkles: feat: add app provider

--- App.tsx
+++ App.tsx
@@ -1,19 +1,12 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+import Routes from "./src/routes";
+import { Provider } from "./src/context/PizzaContext";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <Provider>
+      <Routes />
+    </Provider>
   );
 }

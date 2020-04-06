d9ea9290fec189eb8943ff904ae1805dd2f4cdca
Italo <italo_bruno91@hotmail.com>
2020-04-06 09:30:01 -0300

    :bento: feat: add pizza images

---
+++ assets/images.ts
@@ -0,0 +1,8 @@
+const images = {
+  logotype: require("./img/logotype.png"),
+  pizza: require("./img/pizza.png"),
+  pizzaOutline: require("./img/pizza-outline.png"),
+  pizzaSideOutline: require("./img/pizza-side-outline.png"),
+};
+
+export default images;

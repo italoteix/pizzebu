240914363c9ffd07a86a22069bb146a0eba86549
Italo <italo_bruno91@hotmail.com>
2020-04-06 12:49:27 -0300

    :sparkles: feat: add colors

---
+++ src/constants/colors.ts
@@ -0,0 +1,12 @@
+const ORANGE = "#ffc102";
+const BRIGHT_ORANGE = "#ff6900";
+const BLACK = "#000";
+const DARK_GREY = "#4c4c4c";
+const LIGHT_GREY = "#eee";
+export const WHITE = "#fff";
+
+export const PRIMARY_COLOR = ORANGE;
+export const SECONDARY_COLOR = BRIGHT_ORANGE;
+export const SHADOW_COLOR = BLACK;
+export const PRIMARY_TEXT_COLOR = DARK_GREY;
+export const SLIDER_COLOR = LIGHT_GREY;

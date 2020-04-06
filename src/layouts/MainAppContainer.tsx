f7de0e2013f213c3bfa5177345998791e2defc8b
Italo <italo_bruno91@hotmail.com>
2020-04-06 12:53:27 -0300

    :recycle: refactor: use dimension and color constants

--- src/layouts/MainAppContainer.tsx
+++ src/layouts/MainAppContainer.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
-import { APP_SIDE_MARGIN } from "../constants/dimensions";
+import { APP_SIDE_MARGIN, FONT_SIZES } from "../constants/dimensions";
+import {
+  PRIMARY_COLOR,
+  PRIMARY_TEXT_COLOR,
+  SHADOW_COLOR,
+  WHITE,
+} from "../constants/colors";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#ffc100",
+    backgroundColor: PRIMARY_COLOR,
   },
   headerContent: {
     flex: 1,
@@ -15,13 +21,13 @@ const styles = StyleSheet.create({
     flex: 2,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#fff",
+    backgroundColor: WHITE,
     borderTopLeftRadius: 30,
     borderTopRightRadius: 30,
     paddingHorizontal: APP_SIDE_MARGIN,
     paddingBottom: APP_SIDE_MARGIN * 1.3,
     zIndex: 2,
-    shadowColor: "#000",
+    shadowColor: SHADOW_COLOR,
     shadowOffset: {
       width: 0,
       height: 1,
@@ -30,21 +36,34 @@ const styles = StyleSheet.create({
     shadowRadius: 1.0,
     elevation: 8,
   },
+  title: {
+    color: PRIMARY_TEXT_COLOR,
+    fontSize: FONT_SIZES.xl,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    alignSelf: "flex-start",
+    marginVertical: APP_SIDE_MARGIN,
+  },
 });
 
 export interface MainAppContainerProps {
   mainContent: React.ReactElement;
   headerContent: React.ReactElement;
+  title: string;
 }
 
 const MainAppContainer: React.FC<MainAppContainerProps> = ({
   mainContent,
   headerContent,
+  title,
 }) => {
   return (
     <View style={styles.container}>
       <View style={styles.headerContent}>{headerContent}</View>
-      <View style={styles.mainContent}>{mainContent}</View>
+      <View style={styles.mainContent}>
+        <Text style={styles.title}>{title}</Text>
+        {mainContent}
+      </View>
     </View>
   );
 };

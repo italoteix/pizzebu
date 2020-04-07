2d26ed6ee3f2970240a670e69b124f26012a3872
Italo <italo_bruno91@hotmail.com>
2020-04-07 14:26:52 -0300

    :recycle: refactor: change boilerplate

--- src/screens/BillScreen.tsx
+++ src/screens/BillScreen.tsx
@@ -1,6 +1,41 @@
+/* eslint-disable react/jsx-wrap-multilines */
 import React from "react";
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { StackNavigationProp } from "@react-navigation/stack";
 
-export default () => {
-  return <View />;
+import { StackParamList } from "../routes";
+
+import MainAppContainer from "../layouts/MainAppContainer";
+import DefaultHeader from "../components/DefaultHeader";
+import PrimaryButton from "../components/PrimaryButton";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+});
+
+type BillScreenNavigationProp = StackNavigationProp<StackParamList, "Toppings">;
+
+type Props = {
+  navigation: BillScreenNavigationProp;
 };
+
+const BillScreen = ({ navigation }: Props) => {
+  return (
+    <MainAppContainer
+      title="your pizza bill"
+      headerContent={<DefaultHeader />}
+      mainContent={
+        <View style={styles.container}>
+          <PrimaryButton title="place order" onPress={() => {}} />
+        </View>
+      }
+    />
+  );
+};
+
+export default BillScreen;

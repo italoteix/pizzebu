351b2e450d6b0bfed34fe70b7722d1d324691500
Italo <italo_bruno91@hotmail.com>
2020-04-07 10:29:33 -0300

    :bento: feat: add ingredients imgs

--- assets/images.ts
+++ assets/images.ts
@@ -3,6 +3,16 @@ const images = {
   pizza: require("./img/pizza.png"),
   pizzaOutline: require("./img/pizza-outline.png"),
   pizzaSideOutline: require("./img/pizza-side-outline.png"),
+  pepperoni: require("./img/pepperoni.png"),
+  mushrooms: require("./img/mushrooms.png"),
+  onions: require("./img/onions.png"),
+  sausage: require("./img/sausage.png"),
+  bacon: require("./img/bacon.png"),
+  extraCheese: require("./img/cheese.png"),
+  blackOlives: require("./img/black-olives.png"),
+  greenPeppers: require("./img/green-peppers.png"),
+  pineapple: require("./img/pineapple.png"),
+  spinach: require("./img/spinach.png"),
 };
 
 export default images;

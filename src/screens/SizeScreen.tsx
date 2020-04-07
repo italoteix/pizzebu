0613977f5ea08abc55f2a8810676543f8a487f3f
Italo <italo_bruno91@hotmail.com>
2020-04-07 00:23:50 -0300

    :sparkles: feat: image resize on slide

--- src/screens/SizeScreen.tsx
+++ src/screens/SizeScreen.tsx
@@ -1,18 +1,28 @@
 /* eslint-disable react/jsx-wrap-multilines */
 import React, { useContext, useEffect, useState } from "react";
-import { Image } from "react-native";
+import { Image, StyleSheet, View } from "react-native";
 import { StackNavigationProp } from "@react-navigation/stack";
 
 import images from "../../assets/images";
 import { DEVICE_WIDTH } from "../constants/dimensions";
 import { SIZES } from "../constants/pizzaOptions";
-import { Context, StateType } from "../context/PizzaContext";
+import { Context } from "../context/PizzaContext";
 import { StackParamList } from "../routes";
 
 import MainAppContainer from "../layouts/MainAppContainer";
 import DefaultHeader from "../components/DefaultHeader";
 import Slider from "../components/Slider";
 import PrimaryButton from "../components/PrimaryButton";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  imgContainer: { flex: 1, justifyContent: "center" },
+});
 
 type SizeScreenNavigationProp = StackNavigationProp<StackParamList, "Size">;
 
@@ -21,31 +31,41 @@ type Props = {
 };
 
 const SizeScreen = ({ navigation }: Props) => {
-  const pizza: StateType = useContext(Context);
+  const { state, changePizzaSize } = useContext(Context);
   const [imageSize, setImageSize] = useState(0);
 
   const getImageSize = (size: string | undefined) => {
     const index = SIZES.findIndex((e) => e.short === size);
-    setImageSize(DEVICE_WIDTH * 0.3 + index / 2);
+    const newSize = DEVICE_WIDTH * (0.4 + index / 20);
+    setImageSize(newSize);
   };
 
-  useEffect(() => getImageSize(pizza.size), [pizza.size]);
+  const shortSizes = SIZES.map((size) => size.short);
+
+  useEffect(() => {
+    getImageSize(state.size);
+  }, [state.size]);
   return (
     <MainAppContainer
       title="size"
       headerContent={<DefaultHeader />}
       mainContent={
-        <>
-          <Image
-            source={images.pizzaOutline}
-            style={{ width: imageSize, height: imageSize }}
-          />
-          <Slider />
+        <View style={styles.container}>
+          <View style={styles.imgContainer}>
+            <Image
+              source={images.pizzaOutline}
+              style={{
+                width: imageSize,
+                height: imageSize,
+              }}
+            />
+          </View>
+          <Slider values={shortSizes} onValuesChange={changePizzaSize} />
           <PrimaryButton
             title="next step"
             onPress={() => navigation.navigate("Crust")}
           />
-        </>
+        </View>
       }
     />
   );
